ed0161ffc01514d47a8c7e4743d4616775cf280e
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2021-11-28 10:38:39 +0100

    added preprocessing and loss print

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def get_neighborhood(T,x0,y0,z0,r): # T = original tensor, (x0,y0,z0) central po
 
 
     
+# Main file
+
 if __name__ == '__main__':
     
     # DATA IMPORT 
@@ -27,21 +29,43 @@ if __name__ == '__main__':
     n_src = np.load('../dataset/nsrc_z%.3f.npy' %z) # number of sources per volume\n"
     xi = np.load('../dataset/xHII_z%.3f.npy' %z) # ionization fraction"
     dims = n_igm.shape
+
+    # PREPROCESSING
     
-    net = CNN.CNN()
-    optimizer = optim.SGD(net.parameters(), lr=0.1)
+    n_igm = 2.95 * 10**55 * n_igm
+    mean_n_igm = np.mean(n_igm)
+    std_n_igm = np.std(n_igm)
+    mean_n_src = np.mean(n_src)
+    std_n_src = np.std(n_src)
+
+    n_igm_norm = (n_igm - mean_n_igm) / std_n_igm
+    n_src_norm = (n_src - mean_n_src) / std_n_src
+    
+    net = CNN()
+    optimizer = optim.Adam(net.parameters(), lr=0.1)
+
+    epochs = 10
+
+    output_matrix = np.zeros(dims)
     
     # TRAINING
     
-    for x in range(dims[0]):
-        for y in range(dims[1]):
-            for z in range(dims[2]):
+    for epoch in range(epochs):
+      
+        for x in range(dims[0]):
+            for y in range(dims[1]):
+                for z in range(dims[2]):
                 
-                n_igm_nbh = get_neighborhood(n_igm, x,y,z, r)
-                n_src_nbh = get_neighborhood(n_src, x,y,z, r)
-                loss_fn = nn.CrossEntropyLoss()
-                optimizer.zero_grad()  # set the gradients to 0
-                output = net(n_igm_nbh, n_src_nbh)
-                loss = loss_fn(output, xi[x,y,z])  # compute loss function
-                loss.backward()  # backpropagation
-                optimizer.step()
+                  n_igm_nbh = get_neighborhood(n_igm, x,y,z, r)
+                  n_src_nbh = get_neighborhood(n_src, x,y,z, r)
+                  loss_fn = nn.CrossEntropyLoss()
+                  optimizer.zero_grad()  # set the gradients to 0
+                  output= net(n_igm_nbh, n_src_nbh)
+                  output_matrix[x,y,z] = output.numpy()
+                  loss = loss_fn(output, xi[x,y,z])  # compute loss function
+                  loss.backward()  # backpropagation
+                  optimizer.step()
+
+
+    print("Epoch {:.2f} | Training loss: {:.5f}".format(epoch, loss))
+
