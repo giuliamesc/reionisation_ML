5bfb5e88a51ed90b0418a0599d9ea9dcc900cbd3
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2021-11-28 17:33:38 +0100

    MSELoss and some fixes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+# Main file
+
+# Package loading
 import numpy as np
-import CNN
+import torch
 from torch import nn
 from torch import optim
 
-
-
+# Extracting the neighborhood (of radius r) of the point with coordinates (x0,y0,z0)
 
 def get_neighborhood(T,x0,y0,z0,r): # T = original tensor, (x0,y0,z0) central point, r = radius 
     
@@ -12,11 +14,10 @@ def get_neighborhood(T,x0,y0,z0,r): # T = original tensor, (x0,y0,z0) central po
     idy = range(y0-r,y0+r+1)
     idz = range(z0-r,z0+r+1)
     neigh = T[idx,:,:][:,idy,:][:,:,idz]
+    neigh = torch.from_numpy(neigh)
+    neigh = neigh[None, None, :,:,:]
     return neigh
 
-
-    
-# Main file
 
 if __name__ == '__main__':
     
@@ -25,9 +26,9 @@ if __name__ == '__main__':
     z = 8.397
     r = 24
     
-    n_igm = np.load('../dataset/rho_z%.3f.npy' %z)  # density of intergalactic medium\n"
-    n_src = np.load('../dataset/nsrc_z%.3f.npy' %z) # number of sources per volume\n"
-    xi = np.load('../dataset/xHII_z%.3f.npy' %z) # ionization fraction"
+    n_igm = np.load('rho_z%.3f.npy' %z)  # density of intergalactic medium\n"
+    n_src = np.load('nsrc_z%.3f.npy' %z) # number of sources per volume\n"
+    xi = np.load('xHII_z%.3f.npy' %z) # ionization fraction"
     dims = n_igm.shape
 
     # PREPROCESSING
@@ -42,7 +43,7 @@ if __name__ == '__main__':
     n_src_norm = (n_src - mean_n_src) / std_n_src
     
     net = CNN()
-    optimizer = optim.Adam(net.parameters(), lr=0.1)
+    optimizer = optim.SGD(net.parameters(), lr=0.1)
 
     epochs = 10
 
@@ -56,15 +57,20 @@ if __name__ == '__main__':
             for y in range(dims[1]):
                 for z in range(dims[2]):
                 
-                  n_igm_nbh = get_neighborhood(n_igm, x,y,z, r)
-                  n_src_nbh = get_neighborhood(n_src, x,y,z, r)
-                  loss_fn = nn.CrossEntropyLoss()
-                  optimizer.zero_grad()  # set the gradients to 0
-                  output= net(n_igm_nbh, n_src_nbh)
-                  output_matrix[x,y,z] = output.numpy()
-                  loss = loss_fn(output, xi[x,y,z])  # compute loss function
-                  loss.backward()  # backpropagation
-                  optimizer.step()
+                    n_igm_nbh = get_neighborhood(n_igm, x,y,z, r)
+                    n_src_nbh = get_neighborhood(n_src, x,y,z, r)
+                    loss_fn = torch.nn.MSELoss()
+                    optimizer.zero_grad()  # set the gradients to 0
+                    output= net(n_igm_nbh.float(), n_src_nbh.float())
+                    temp = torch.Tensor.detach(output)
+                    to_output_matrix = torch.Tensor.numpy(temp)
+                    output_matrix[x,y,z] = to_output_matrix
+                    print(output.shape)
+                    estimation = torch.Tensor([xi[x,y,z]])
+                    print(estimation)
+                    loss = loss_fn(output, estimation)  # compute loss function
+                    loss.backward()  # backpropagation
+                    optimizer.step()
 
 
     print("Epoch {:.2f} | Training loss: {:.5f}".format(epoch, loss))
