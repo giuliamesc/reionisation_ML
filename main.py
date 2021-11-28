afc495e880f6c288af88bc39d5d87fbdefff3a45
Matteo Calafa <matteo.calafa@mail.polimi.it>
2021-11-28 11:57:06 +0000

    Fixed dimensions, same error...

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import CNN
+import torch
 from torch import nn
 from torch import optim
 
@@ -11,8 +12,27 @@ def get_neighborhood(T,x0,y0,z0,r): # T = original tensor, (x0,y0,z0) central po
     idx = range(x0-r,x0+r+1)
     idy = range(y0-r,y0+r+1)
     idz = range(z0-r,z0+r+1)
-    neigh = T[idx,:,:][:,idy,:][:,:,idz]
+    
+    Tm = T[0,0,:,:,:]
+    neigh = Tm[idx,:,:][:,idy,:][:,:,idz]
+    neigh = np.expand_dims(neigh, axis=0)
+    neigh = np.expand_dims(neigh, axis=0)
     return neigh
+
+
+
+def preprocessing(n_igm,n_src):
+    
+    n_igm = 2.95 * 10**55 * n_igm
+    mean_n_igm = np.mean(n_igm)
+    std_n_igm = np.std(n_igm)
+    mean_n_src = np.mean(n_src)
+    std_n_src = np.std(n_src)
+
+    n_igm = (n_igm - mean_n_igm) / std_n_igm
+    n_src = (n_src - mean_n_src) / std_n_src
+    return n_igm, n_src
+    
 
 
     
@@ -30,23 +50,25 @@ if __name__ == '__main__':
     xi = np.load('../dataset/xHII_z%.3f.npy' %z) # ionization fraction"
     dims = n_igm.shape
 
-    # PREPROCESSING
-    
-    n_igm = 2.95 * 10**55 * n_igm
-    mean_n_igm = np.mean(n_igm)
-    std_n_igm = np.std(n_igm)
-    mean_n_src = np.mean(n_src)
-    std_n_src = np.std(n_src)
 
-    n_igm_norm = (n_igm - mean_n_igm) / std_n_igm
-    n_src_norm = (n_src - mean_n_src) / std_n_src
+    # PREPROCESSING
+    n_igm, n_src = preprocessing(n_igm, n_src)
     
-    net = CNN()
+    
+    net = CNN.CNN()
     optimizer = optim.Adam(net.parameters(), lr=0.1)
 
     epochs = 10
 
     output_matrix = np.zeros(dims)
+    
+    n_igm = np.expand_dims(n_igm, axis=0)
+    n_igm = np.expand_dims(n_igm, axis=0)
+    n_src = np.expand_dims(n_src, axis=0)
+    n_src = np.expand_dims(n_src, axis=0)
+    
+    
+    
     
     # TRAINING
     
@@ -55,9 +77,8 @@ if __name__ == '__main__':
         for x in range(dims[0]):
             for y in range(dims[1]):
                 for z in range(dims[2]):
-                
-                  n_igm_nbh = get_neighborhood(n_igm, x,y,z, r)
-                  n_src_nbh = get_neighborhood(n_src, x,y,z, r)
+                  n_igm_nbh = torch.tensor(get_neighborhood(n_igm, x,y,z, r))
+                  n_src_nbh = torch.tensor(get_neighborhood(n_src, x,y,z, r))
                   loss_fn = nn.CrossEntropyLoss()
                   optimizer.zero_grad()  # set the gradients to 0
                   output= net(n_igm_nbh, n_src_nbh)
