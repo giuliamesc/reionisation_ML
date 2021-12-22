e2196ab67b11e7cc9f3bc7ac2882df90add931cf
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2021-12-22 20:47:57 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,13 +1,15 @@
 import numpy as np, matplotlib.pyplot as plt
 import pickle
 
+# reading data to plot
 path = './checkpoints/'
 loss, val_loss = pickle.load(open("%sloss_train.txt" %path, "rb"))['train_loss'], pickle.load(open("%sloss_test.txt" %path, "rb"))['test_loss']
 R2, val_R2 = pickle.load(open("%sR2_train.txt" %path, "rb"))['R2_train'], pickle.load(open("%sR2_test.txt" %path, "rb"))['R2_test']
 
+# extracting the number of epoch with best performance
 idx_best_mode = np.argmin(val_loss)
 
-# Plot
+# plot
 fig, ax = plt.subplots(1, 2, figsize=(16,8))
 ax[0].plot(loss, label='train')
 ax[0].plot(val_loss, label='test')
@@ -21,4 +23,4 @@ ax[1].plot(idx_best_mode, val_R2[idx_best_mode], 'rx')
 ax[1].set_ylim(-0.1,1)
 ax[1].set_xlabel('epochs'), ax[1].set_ylabel(r'$R^2$')
 
-plt.savefig('%sloss_plot.png' %path, bbox_inches='tight')
+plt.savefig('%sloss_plot.png' %path, bbox_inches='tight') # saving the plot
